9d73c8031ddbe644d7572e33dc105847d7973708
Guilherme da Silva Benevides <git@gui.dev.br>
2026-07-08 21:47:40 -0300

    feat: disable auth when secret missing

--- src/services/AuthService.ts
+++ src/services/AuthService.ts
@@ -1,9 +1,12 @@
 import { getEnv } from "../utils/getEnv";
 
-const AUTH_SECRET = getEnv("AUTH_SECRET");
+const AUTH_SECRET = getEnv("AUTH_SECRET", false, "");
 
 export class AuthService {
-	static verify(secret: string) {
+	static verify(secret?: string) {
+		if (!AUTH_SECRET) {
+			return true;
+		}
 		return secret === AUTH_SECRET;
 	}
 }
